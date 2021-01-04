2cb69592ae400fcc785f6f37a4fb291507851b5b
Jason Howe <trisynth@gmail.com>
2021-01-04 13:40:35 -0500

    CORS fix?

--- modules/app/app.js
+++ modules/app/app.js
@@ -3,7 +3,11 @@ const cors = require('cors');
 
 const app = express();
 
-// app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  })
+);
 
 app.use('/people', require('../people/people.router'));
 app.use('/pets', require('../pets/pets.router'));
